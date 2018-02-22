40f5d4bd88d876ccad0f177c609af655d8db1cf5
Zain Zia <zainzia0341@gmail.com>
2018-02-22 19:38:26 +0500

    Completed todo app

--- javaScript/app.js
+++ javaScript/app.js
@@ -32,29 +32,25 @@ function AddTodo(){
         document.getElementsByClassName("EditCover")[0].style.display ="block";
         document.getElementsByClassName("EditContainer")[0].setAttribute("id","EditAnimation");     
         var newedit = this.parentElement;
-        document.getElementsByTagName("textarea")[0].value = newedit.childNodes[1].innerHTML;
+        var editElement = newedit.childNodes[0].innerText;
+        saveEditElement = newedit.childNodes[0];
+        document.getElementsByTagName("textarea")[0].value = editElement;
         }
     }
-}
-var close = document.getElementsByClassName("glyphicon-trash");
-var i;
-for (i = 0; i < close.length; i++) {
-close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-   }
-}
-var edit = document.getElementsByClassName("glyphicon-pencil");
-var i;
-for (i = 0; i < edit.length; i++) {
-edit[i].onclick = function() {
-    document.getElementsByClassName("EditCover")[0].style.display ="block";
-    document.getElementsByClassName("EditContainer")[0].setAttribute("id","EditAnimation");     
-    var newedit = this.parentElement;
-    document.getElementsByTagName("textarea")[0].value = newedit.childNodes[1].innerHTML;
-   }
 }
 function cancelEdit(){
     document.getElementsByClassName("EditCover")[0].style.display ="none";
     document.getElementsByClassName("EditContainer")[0].setAttribute("id","EditAnimation1");     
+}
+function Edit(){
+     var editContent = document.getElementsByTagName("textarea")[0].value;
+     editElement = editContent;
+     console.log(editElement);
+     console.log(editContent);
+     saveEditElement.innerText=editContent;
+     document.getElementsByClassName("EditCover")[0].style.display ="none";
+     document.getElementsByClassName("EditContainer")[0].setAttribute("id","EditAnimation1");
+}
+function clearAll(){
+    var mainCon = document.getElementById("mainContainer").innerText="";
 }
